1bbad430a66c06cec340ab7a1d97e4b5841353c1
Nico Jensch <root@dr460nf1r3.org>
2023-12-28 14:44:35 +0100

    ci(schedule-package): adapt for GitHub repo running GitLab CI
    Especially to account for the case of multiple commits getting pulled by GitLab at once, resulting in only one pipeline run, using the latest commit. To still have all changes scheduled, we use a special 'scheduled' tag which gets updated after scheduling all relevant packages

--- .ci/schedule-package.sh
+++ .ci/schedule-package.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
 function prepare-env() {
-    if [[ "$CI_COMMIT_MESSAGE" == *"[deploy"*"]"* ]]; then
-        _FROM_COMMIT_MSG=1
-    else
-        _FROM_COMMIT_MSG=0
-    fi
+    # Get all commit messages between "scheduled" tag and HEAD to get all relevant
+    # ones between 2 pipeline runs. This is required because the webhook execution
+    # for pulling commits to GitLab CI doesn't seem to be running instantly, resulting
+    # in only one pipeline run for multiple commits
+    mapfile -t _CURRENT_COMMIT_MSG < <(git log --pretty=format:"commit=%h message=%s" scheduled..HEAD)
 
-    # Parse our commit message for which packages to build based on folder names
+    # Determine which commits to parse how
+    for commit in "${_CURRENT_COMMIT_MSG[@]}"; do
+        if [[ "$commit" == *"[deploy"*"]"* ]]; then
+            _DEPLOY_VIA_COMMIT_MSG+=("$commit")
+        else
+            _DEPLOY_VIA_GIT_DIFF+=("$commit")
+        fi
+    done
+
+    # Build a list of valid packages
     mapfile -t _PACKAGES < <(find . -mindepth 1 -type d -prune | sed -e '/.\./d' -e 's/.\///g')
 }
 
 function parse-commit() {
-    if [[ "$_FROM_COMMIT_MSG" == 1 ]]; then
-        if [[ "$CI_COMMIT_MESSAGE" == *"[deploy all]"* ]]; then
+    for commit in "${_DEPLOY_VIA_COMMIT_MSG[@]}"; do
+        if [[ "$commit" == *"[deploy all]"* ]]; then
             for package in "${_PACKAGES[@]}"; do
                 _PKG+=("chaotic-aur:$package")
             done
             echo "Requested a full routine run."
-        elif [[ "$CI_COMMIT_MESSAGE" == *"[deploy"*"]"* ]]; then
+        elif [[ "$commit" == *"[deploy"*"]"* ]]; then
             for package in "${_PACKAGES[@]}"; do
                 if [[ "$CI_COMMIT_MESSAGE" == *"[deploy $package]"* ]]; then
                     _PKG=("chaotic-aur:$package")
@@ -30,15 +39,19 @@ function parse-commit() {
         else
             echo "No package to build found in commit message. Exiting." && exit 1
         fi
-    else
-        return 0
-    fi
+    done
 }
 
 parse-gitdiff() {
-    if [[ "$_FROM_COMMIT_MSG" == 0 ]]; then
-        # Compare the last 2 commits
-        _CURRENT_DIFF=$(git --no-pager diff --name-only HEAD~1..HEAD)
+    for commit in "${_DEPLOY_VIA_GIT_DIFF[@]}"; do
+        # Extract commit ID
+        local _CURRENT_COMMIT
+        _CURRENT_COMMIT=$(echo "$commit" | grep -oP '^commit=\K[0-9a-f]{7}')
+
+        # Compare differences between our current commit and the commit before it to get
+        # a list of changes
+        local _CURRENT_DIFF
+        _CURRENT_DIFF=$(git --no-pager diff --name-only "$_CURRENT_COMMIT"~1.."$_CURRENT_COMMIT")
 
         # Check whether relevant folders got changed
         for package in "${_PACKAGES[@]}"; do
@@ -47,22 +60,29 @@ parse-gitdiff() {
                 echo "Detected changes in $package, scheduling build..."
             fi
         done
-
-        if [[ "${#_PKG[@]}" == 0 ]]; then
-            echo "No relevant package changes to build found, exiting gracefully." && exit 0
-        fi
-    else
-        return 0
-    fi
+    done
 }
 
 schedule-package() {
+    if [[ "${#_PKG[@]}" == 0 ]]; then
+        echo "No relevant package changes to build found, exiting gracefully." && exit 0
+    fi
+
+    # To only schedule each package once, strip all duplicates
+    mapfile -t _FINAL_PKG < <(for pkg in "${_PKG[@]}"; do echo "$pkg"; done | sort -u)
+
     # Schedule either a full run or a single package using chaotic-manager
     # the entry_point script also establishes a connection to our Redis server
-    /entry_point.sh schedule --commit "${CI_COMMIT_SHA}:${CI_PIPELINE_ID}" --repo "$BUILD_REPO" "${_PKG[@]}"
+    /entry_point.sh schedule --commit "${CI_COMMIT_SHA}:${CI_PIPELINE_ID}" --repo "$BUILD_REPO" "${_FINAL_PKG[@]}"
+}
+
+update-tag() {
+    git tag -m "All PKGBUILD changes up to this tag have been scheduled." -f scheduled
+    git push --tags -f "$REPO_URL" HEAD:main
 }
 
 prepare-env
 parse-commit
 parse-gitdiff
 schedule-package
+update-tag
